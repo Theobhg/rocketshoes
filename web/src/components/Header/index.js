7cd3c64dd4270c0061abadcde7c9ef6d398aa651
Theobhg <assis.theo@gmail.com>
2020-05-19 14:44:21 -0300

    add route links/cart-component/import-reacticons

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { MdShoppingCart } from 'react-icons/md';
 
 import { Container } from './styles';
 
@@ -7,7 +10,17 @@ import logo from '../../assets/images/logo.svg';
 export default function Header() {
   return (
     <Container>
-      <img src={logo} alt="Rocketshoes" />
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>My Cart</strong>
+          <span>3 items</span>
+        </div>
+        <MdShoppingCart size={36} color="#FFF" />
+      </Cart>
     </Container>
   );
 }
