475254670dfba9ffb35678db8b876372c68278cd
Theobhg <assis.theo@gmail.com>
2020-05-19 18:10:10 -0300

    styling homepage

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -22,6 +22,91 @@ export default function Home() {
           <span>ADD TO CART</span>
         </button>
       </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
+          alt="Sneakers"
+        />
+        <strong>Confortable sneaker</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
+          alt="Sneakers"
+        />
+        <strong>Confortable sneaker</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
+          alt="Sneakers"
+        />
+        <strong>Confortable sneaker</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
+          alt="Sneakers"
+        />
+        <strong>Confortable sneaker</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
+          alt="Sneakers"
+        />
+        <strong>Confortable sneaker</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
     </ProductList>
   );
 }
