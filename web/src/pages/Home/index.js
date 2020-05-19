75a1183618fc2684f83f55fc8d9f52d476951829
Theobhg <assis.theo@gmail.com>
2020-05-19 16:26:12 -0300

    add component ProductList/img=test/md-icon

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
-function Home() {
-  return <h1>Home</h1>;
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
+          alt="Sneakers"
+        />
+        <strong>Confortable sneaker</strong>
+        <span>R$129,00</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
-
-export default Home;
