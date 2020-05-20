31e83d92409ad72763d2ebc7eb26fdd975d00a93
Theobhg <assis.theo@gmail.com>
2020-05-20 11:16:11 -0300

    getting products/img/title/price  from api

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -5,25 +5,37 @@ import api from '../../services/api';
 import { ProductList } from './styles';
 
 export default class Home extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    this.setState({ products: response.data });
+  }
+
   render() {
+    const { products } = this.state;
+
     return (
       <ProductList>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
-            alt="Sneakers"
-          />
-          <strong>Confortable sneaker</strong>
-          <span>R$129,00</span>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
 
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#fff" /> 3
-            </div>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
 
-            <span>ADD TO CART</span>
-          </button>
-        </li>
+              <span>ADD TO CART</span>
+            </button>
+          </li>
+        ))}
       </ProductList>
     );
   }
