804084df905efe390f11cce20822883acc423754
Theobhg <assis.theo@gmail.com>
2020-05-20 11:01:30 -0300

    import api/classcomponent/api.get

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,112 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
-          alt="Sneakers"
-        />
-        <strong>Confortable sneaker</strong>
-        <span>R$129,00</span>
+export default class Home extends Component {
+  render() {
+    return (
+      <ProductList>
+        <li>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
+            alt="Sneakers"
+          />
+          <strong>Confortable sneaker</strong>
+          <span>R$129,00</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 3
+            </div>
 
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
-          alt="Sneakers"
-        />
-        <strong>Confortable sneaker</strong>
-        <span>R$129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
-          alt="Sneakers"
-        />
-        <strong>Confortable sneaker</strong>
-        <span>R$129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
-          alt="Sneakers"
-        />
-        <strong>Confortable sneaker</strong>
-        <span>R$129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
-          alt="Sneakers"
-        />
-        <strong>Confortable sneaker</strong>
-        <span>R$129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg?ts=1586961574&ims=326x"
-          alt="Sneakers"
-        />
-        <strong>Confortable sneaker</strong>
-        <span>R$129,00</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span>ADD TO CART</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+            <span>ADD TO CART</span>
+          </button>
+        </li>
+      </ProductList>
+    );
+  }
 }
